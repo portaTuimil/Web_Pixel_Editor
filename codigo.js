0ee9d68c5e644dbd6192a12e81870b0d5741a207
Pablo Porta <pabloportatuimil12@gmail.com>
2023-01-29 18:21:49 +0100

    Fill added, project finished

--- codigo.js
+++ codigo.js
@@ -8,7 +8,7 @@ function makeGrid(columns, rows){
         const div = document.createElement('div')
         container.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
         container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
-        container.appendChild(div).classList.add('box')
+        container.appendChild(div).classList.add('box');
 
 
     }
@@ -29,17 +29,20 @@ container.addEventListener("mouseup", function() {
 let pixels = document.querySelectorAll('.box');
         pixels.forEach(pixel=> pixel.addEventListener('mouseover', ()=> {
             if (mouseOn === true){
-            pixel.setAttribute('style', `background: ${color};`);
+            pixel.setAttribute('style', `background: ${colorValue};`);
 }}));
 
-let mode = 'normal';
-let color = 'rgb(1,1,1)'
+let color = document.querySelector('#Color')
+let colorValue = color.value;
+color.addEventListener('input', ()=>{
+    colorValue = color.value;
+})
 
 //Reset:
 let reset = document.querySelector('#Clear');
 reset.addEventListener('click', ()=>{
-    pixels.forEach(pixel =>{
-        pixel.setAttribute('style', 'background: white;');
+    document.querySelectorAll('.box').forEach(pixel =>{
+        pixel.setAttribute(`style`, `background-color: #ffffff;`);
         mouseOn = false;
     })
 });
@@ -64,18 +67,27 @@ gridSize.addEventListener('input', (event)=> {
 //New grid generator:
 gridSize.addEventListener('mouseup', (event)=> {
     GridValue = event.target.value
-    GridSizeValueOutput.textContent = GridValue
+    GridSizeValueOutput.textContent = GridValue;
     makeGrid(GridValue, GridValue);
     
     //Escritor
     let pixels = document.querySelectorAll('.box');
         pixels.forEach(pixel=> pixel.addEventListener('mouseover', ()=> {
             if (mouseOn === true){
-            pixel.setAttribute('style', `background: ${color};`);
+            pixel.setAttribute('style', `background-color: ${colorValue};`);
     }}));
     
     //Reset:
     pixels.forEach(pixel =>{
         pixel.setAttribute('style', 'background: white;');
         mouseOn = false});
-})
+});
+
+//Fill:
+let fill = document.querySelector('#Fill');
+
+fill.addEventListener('click', ()=> {
+    pixels.forEach(pixel =>{
+        pixel.setAttribute('style', `background: ${colorValue};`);
+        mouseOn = false});
+});
